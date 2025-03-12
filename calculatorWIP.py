6f8783c64958bee7a689ac3e5e387c7dd6a11bd9
Ratseki <98866305+Ratseki@users.noreply.github.com>
2025-03-13 07:42:20 +0800

    Add files via upload

---
+++ calculatorWIP.py
@@ -0,0 +1,19 @@
+
+
+print("*** CALCULATOR ****")
+print("Please select a method")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+if 1:
+
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    additionSum = num1 + num2
+
+print(f"The sum is {additionSum}")
+
+# if 2:
